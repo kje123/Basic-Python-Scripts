f9b142a05629bb9296b6e7c36821464470f5a35b
kje123 <keith-3@hotmail.com>
2015-10-27 12:29:11 -0700

    Create basicvarinput.py

---
+++ basicvarinput.py
@@ -0,0 +1,11 @@
+# Basic intro to variables + input functions
+# By Keith Ellingwood
+# twitter.com/3Ellingwood
+# github.com/kje123
+
+var = input()
+# defines variable var as an input. Whatever you input will set var as that input for the rest of the operation
+
+if var == "a":
+    print "yay"
+# asks if var is "a". If so, prints the output "yay"
